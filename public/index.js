3a0bedea4d8d4b2c386f242407571073e1ffa75b
huxiang <huxianga@yonyou.com>
2018-03-08 17:24:39 +0800

    简化index.html

--- public/index.js
+++
@@ -1,9 +0,0 @@
-function showUserName (resp) {
-    return document.querySelector('input[name="username"]').value = resp.username || '';
-}
-
-document.getElementById('getUser').addEventListener('click', function () {
-    axios.get('api/user').then(function (resp) {
-        showUserName(resp.data)
-    })
-})
